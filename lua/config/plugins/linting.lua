5bdff66938039910beffbbba7f26dcfb7565c1fb
Daniel Roberto <sdanirobert@outlook.com>
2024-06-22 15:54:24 -0300

    fix eslint message

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -1,29 +1,51 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
-    }
+		local function has_eslint_config()
+			local eslintrc_files = {
+				".eslintrc",
+				".eslintrc.json",
+				".eslintrc.js",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+			}
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			for _, file in ipairs(eslintrc_files) do
+				if vim.fn.glob(file) ~= "" then
+					return true
+				end
+			end
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+			return false
+		end
 
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			python = { "pylint" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				if vim.bo.filetype == "python" or has_eslint_config() then
+					lint.try_lint()
+				end
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			if vim.bo.filetype == "python" or has_eslint_config() then
+				lint.try_lint()
+			end
+		end, { desc = "Trigger linting for current file" })
+	end,
 }
