94eb7945a05ded144cd5f0a09ee8b297b760a88b
Daniel Roberto <sdanirobert@outlook.com>
2024-10-12 10:45:13 -0300

    fix: minor tweaks

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,5 +3,5 @@
 -- ================================
 
 -- Load custom configuration modules
-require("config.options") -- General settings and options
+require("config.settings") -- General settings and options
 require("config.keymaps") -- Key mapping configurations
