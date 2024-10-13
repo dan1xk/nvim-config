4bd7d84952dff3b30fd12779ee35554f5226bf1d
Daniel Roberto <sdanirobert@outlook.com>
2024-10-13 01:21:05 -0300

    feat: add surround and neoscroll plugins

---
+++ lua/config/plugins/neoscroll.lua
@@ -0,0 +1,10 @@
+-- ================================
+--        Neoscroll  Plugin
+-- ================================
+
+return {
+	"karb94/neoscroll.nvim",
+	config = function()
+		require("neoscroll").setup({})
+	end,
+}
