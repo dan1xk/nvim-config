13d68cb398c7ce10ffe0d788a20919cd98397556
Daniel Roberto <sdanirobert@outlook.com>
2024-10-12 23:54:35 -0300

    fix: adjust linting.lua

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -8,7 +8,6 @@ return {
 	config = function()
 		local lint = require("lint")
 
-		-- Define linters based on file types
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
@@ -18,10 +17,8 @@ return {
 			python = { "pylint" },
 		}
 
-		-- Create an augroup for linting
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		-- Autocommand to trigger linting on specific events
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
@@ -29,10 +26,8 @@ return {
 			end,
 		})
 
-		-- Key mapping to manually trigger linting
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
 	end,
 }
-
