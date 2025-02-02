fb821a8f7b12d100bd4437354c59140abb9fc503
Daniel Roberto <sdanirobert@outlook.com>
2025-02-02 14:18:08 -0300

    add eslint in conform plugin

--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -11,17 +11,15 @@ return {
 		-- Setup conform with specified formatters
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" }, -- Format JavaScript files with Prettier
-				typescript = { "prettier" }, -- Format TypeScript files with Prettier
-				javascriptreact = { "prettier" }, -- Format React files with Prettier
-				typescriptreact = { "prettier" }, -- Format TypeScript React files with Prettier
-				svelte = { "prettier" }, -- Format Svelte files with Prettier
+				javascript = { "eslint_d", "prettier" }, -- Format JavaScript files with Prettier
+				typescript = { "eslint_d", "prettier" }, -- Format TypeScript files with Prettier
+				javascriptreact = { "eslint_d", "prettier" }, -- Format React files with Prettier
+				typescriptreact = { "eslint_d", "prettier" }, -- Format TypeScript React files with Prettier
 				css = { "prettier" }, -- Format CSS files with Prettier
 				html = { "prettier" }, -- Format HTML files with Prettier
 				json = { "prettier" }, -- Format JSON files with Prettier
 				yaml = { "prettier" }, -- Format YAML files with Prettier
 				markdown = { "prettier" }, -- Format Markdown files with Prettier
-				graphql = { "prettier" }, -- Format GraphQL files with Prettier
 				liquid = { "prettier" }, -- Format Liquid files with Prettier
 				lua = { "stylua" }, -- Format Lua files with Stylua
 				python = { "isort", "black" }, -- Format Python files with isort and Black
@@ -40,6 +38,6 @@ return {
 				async = false,
 				timeout_ms = 500,
 			})
-		end, { desc = "Format file or range (in visual mode)" }) -- Description for the key mapping
+		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
